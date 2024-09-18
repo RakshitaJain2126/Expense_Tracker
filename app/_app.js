792f5a886f256f60f973ddd8470379a6f6f6d715
RakshitaJain2126 <rakshitajain2108@gmail.com>
2024-09-18 18:39:55 +0530

    Made changes in _app.js

--- app/_app.js
+++ app/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
             <script async src="https://www.googletagmanager.com/gtag/js?id=GA_ID"></script>
             <script>
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){dataLayer.push(arguments)};
                 gtag('js', new Date());
 
                 gtag('config', 'GA_ID');
